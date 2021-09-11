a178b6801b41ad26d1cd14042ca341c68162957b
shubham nain <49475581+snain@users.noreply.github.com>
2021-09-12 02:30:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+    console.log(__dirname);
+});
+
+
+
+app.post("/",function(req,res){
+    var n1=Number(req.body.num1);
+    var n2=Number(req.body.num2);
+    var result=n1/(n1*n2);
+    console.log(req.body);
+    res.send("Answer is "+result);
+    
+});
+ 
+
+app.listen(3000,function(){
+    console.log("server is working on port 3000");
+});
